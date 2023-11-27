fe5c44e467c361e78793ebc0cd3572ea3e155842
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-11-27 11:27:37 +0400

    Reset or cancel the timer

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text ="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text = "")
+    global  reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
@@ -41,7 +48,8 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000,count_down, count - 1)
+        global  timer
+        timer = window.after(1000,count_down, count - 1)
     else:
         start_timer()
         marks = ""
@@ -79,10 +87,10 @@ start_button = Button(text="Start",highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 
-reset_button = Button(text="Reset",highlightthickness=0)
+reset_button = Button(text="Reset",highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW )
+check_marks = Label(fg=GREEN, bg=YELLOW )
 check_marks.grid(column=1,row=3)
 
 window.mainloop()
